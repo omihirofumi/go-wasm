ad35ffdd379653563ef59bdb4e90b13341bca1b6
hirofumi omi <omihirofumi@gmail.com>
2023-08-17 18:16:24 +0900

    compressor

--- main.go
+++ main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"github.com/omihirofumi/go-wasm/compress"
+	"io"
 	"syscall/js"
 )
 
 func main() {
-	window := js.Global()
-	document := window.Get("document")
-	body := document.Get("body")
-	btn := document.Call("createElement", "button")
-	btn.Set("textContent", "click me!")
-	btn.Call("addEventListener", "click", js.FuncOf(func(js.Value, []js.Value) interface{} {
-		fmt.Println("Hello, WebAssembly!")
-		return nil
-	}))
-	body.Call("appendChild", btn)
-	select {}
+	var compressFunc = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		jsSrc := args[0]
+		srcLen := jsSrc.Get("length").Int()
+		srcBytes := make([]byte, srcLen)
+		js.CopyBytesToGo(srcBytes, jsSrc)
+
+		src := bytes.NewReader(srcBytes)
+
+		r, err := compress.Compress(src)
+		if err != nil {
+			panic(err)
+		}
+
+		var buf bytes.Buffer
+		if _, err := io.Copy(&buf, r); err != nil {
+			panic(err)
+		}
+		ua := newUnit8Array(buf.Len())
+		js.CopyBytesToJS(ua, buf.Bytes())
+		return ua
+	})
+	js.Global().Set("compress_test", compressFunc)
+}
+
+func newUnit8Array(size int) js.Value {
+	ua := js.Global().Get("Uint8Array")
+	return ua.New(size)
 }
